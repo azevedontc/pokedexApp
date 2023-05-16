ae2a9c4336241ce61aa5197bfdc27dc207d3512b
Azevedo <ubaporangacity@gmail.com>
2023-05-16 20:09:21 -0300

    progress-bar

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,21 @@
 const buttonTheme = document.getElementById("button-theme");
 const buttonImg = document.querySelector(".button");
 const body = document.querySelector("body");
+
+window.addEventListener('scroll', updateProgressBar);
+
+// progress-bar
+function updateProgressBar() {
+  const progressBar = document.getElementById('progress-bar');
+  const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  const fullHeight = document.documentElement.scrollHeight;
+  const scrolledHeight = window.pageYOffset || document.documentElement.scrollTop;
+  const scrollPercentage = (scrolledHeight / (fullHeight - windowHeight)) * 100;
+
+  progressBar.style.width = scrollPercentage + '%';
+}
+
+// night-mode
 buttonTheme.addEventListener("click", () => {
     const nightmodeOn = body.classList.contains("night-mode");
 
